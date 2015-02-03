8a235e2110f8af721c770df06c728fddf62b6265
Ilya Krasnov <ilya.krasnov@gmail.com>
2015-02-03 09:08:46 +0100

    Add gender toggle

--- App/app.js
+++ App/app.js
@@ -1,6 +1,16 @@
 angular.module('madLibs',[])
   .controller('MadCtrl', ['$scope', function($scope) {
     $scope.inputs = {
-      femaleName:'Female name!'
+      name:'Name',
+      jobTitle:'Job Title',
+      tediousTask:'Tedious Task',
+      dirtyTask:'Dirty Task',
+      celebrity:'Celebrity',
+      uselessSkill:'Useless Skill',
+      adjective:'Adjective',
+      obnoxiuousCelbertity:'Obnoxiuous Celbertity',
+      hugeNumber:'Huge Number'
     };
+    $scope.gender = 'Female';
   }]);
+
